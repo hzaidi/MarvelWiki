40b1f88f6e6d4bd3dfc8400259dd8e7ac4d154bd
Hamza Zaidi <humza.zaidi@gmail.com>
2018-05-18 12:53:06 -0400

    Add node packages to lambda function

---
+++ lambdaFunctions/apiKey.js
@@ -0,0 +1,11 @@
+const fetch = require('node-fetch');
+const md5 = require('md5');
+const PUBLIC_KEY = '811b2529a0130c92ab5c1a36e00c61e5';
+const PRIVATE_KEY = '426771ca28a1e77bfa361d0c09731bdadf0b63e3';
+
+exports.handler = function(event, context, callback) {
+    callback(null, {
+		statusCode: 200,
+		body: md5("Hello, World")
+	});
+}

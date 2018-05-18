0285dbc4abc8edb9788dbce1a25b04e086e0fcd0
Hamza Zaidi <humza.zaidi@gmail.com>
2018-05-18 12:55:39 -0400

    Install MD5 dependency

--- lambdaFunctions/apiKey.js
+++ lambdaFunctions/apiKey.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
 const md5 = require('md5');
 const PUBLIC_KEY = '811b2529a0130c92ab5c1a36e00c61e5';
 const PRIVATE_KEY = '426771ca28a1e77bfa361d0c09731bdadf0b63e3';
